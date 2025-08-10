46b1cbb933beb6a9742dd6a271903cf3f211aa30
rathodVinay <118358113+rathodVinay@users.noreply.github.com>
2025-08-10 21:00:14 +0530

    Update App.js with main app component

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import './index.css';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
